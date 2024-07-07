2c164a8d4eb725e9aeeb8d4e3dc20163070af102
ello-lithuania <63595651+ello-lithuania@users.noreply.github.com>
2024-07-07 15:15:47 +0300

    fix: deployment url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ello-lithuania-REACT2024Q3/',
+  base: '/react-class_components/',
   plugins: [react()],
 })
